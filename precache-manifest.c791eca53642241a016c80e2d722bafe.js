76bc22bc1ef6e9b8c0bb6d23aaedf1b970e7c30e
Maire Chew <mairechew@gmail.com>
2019-07-25 08:52:05 -0600

    Updates

---
+++ precache-manifest.c791eca53642241a016c80e2d722bafe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf97da7a8b0c2cf7ffec72c6023345ac",
+    "url": "/mairechew/deploy-react/index.html"
+  },
+  {
+    "revision": "8958e499480c70e47f12",
+    "url": "/mairechew/deploy-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/mairechew/deploy-react/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "8958e499480c70e47f12",
+    "url": "/mairechew/deploy-react/static/js/main.c93049d1.chunk.js"
+  },
+  {
+    "revision": "3d3513a4c52f2dc059e7",
+    "url": "/mairechew/deploy-react/static/js/runtime~main.74dfc76f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mairechew/deploy-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
